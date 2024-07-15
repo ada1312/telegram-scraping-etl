0eefb6fe6b7878c19e6c1c48b21f13bad57a27c3
ada1312 <ajda@beelabs.dev>
2024-07-15 23:37:55 +0200

    changes because of too big terraform file

--- src/deployment/terraform/environments/test/main.tf
+++ src/deployment/terraform/environments/test/main.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad6716daf732dd770c490cae870425aeffe650fd16097971c437ecda67ed5101
-size 3082
+oid sha256:94bfbdf17fd23d3058ee764723a9d6f8624576944e9e50aadeef7f17ad1e7c35
+size 3071
